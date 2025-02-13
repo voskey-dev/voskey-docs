aa0d3eaa960439e90345c439d96fbd25193d9365
uboar <uboaruboar@gmail.com>
2025-02-13 21:47:06 +0900

    ランディングページの作成(仮)

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,10 @@ export default defineConfig({
 		// },
 		sidebar: [
 			{
+				label: 'ランディングページ',
+				slug: 'landing'
+			},
+			{
 				label: '利用規約・プライバシーポリシー',
 				items: [
 					{ label: '利用規約', slug: 'terms/term' },
